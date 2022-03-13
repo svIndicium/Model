84cf4ec1c686fc40c6b14d0dd36dd59eb1341f20
joostlek <joostlek@outlook.com>
2022-03-13 19:38:29 +0100

    Add StudyType

--- src/main/kotlin/hu/indicium/dev/model/member/Member.kt
+++ src/main/kotlin/hu/indicium/dev/model/member/Member.kt
@@ -1,9 +1,12 @@
 package hu.indicium.dev.model.member
 
+import hu.indicium.dev.model.studytype.StudyType
+
 data class Member(
     val id: String,
     val firstName: String,
     val prefix: String?,
     val lastName: String,
     val birthdate: String,
+    val studyType: StudyType?,
 )
---
+++ src/main/kotlin/hu/indicium/dev/model/studytype/StudyType.kt
@@ -0,0 +1,6 @@
+package hu.indicium.dev.model.studytype
+
+data class StudyType(
+    val shortName: String,
+    val name: String,
+)
